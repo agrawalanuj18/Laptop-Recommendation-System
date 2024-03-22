3d95b8d87d970e511f88bda37857909533b7f6a5
Anuj Agrawal <84292570+agrawalanuj18@users.noreply.github.com>
2024-03-22 13:06:57 +0530

    Update ProductList.js

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -37,23 +37,8 @@ const filters = [
       { value: 'Web Development', label: 'Web Development', checked: false },
       { value: 'App Development', label: 'App Development', checked: false },
       { value: 'Programming', label: 'Programming', checked: false },
-      // { value: 'skincare', label: 'skincare', checked: false },
-      // { value: 'groceries', label: 'groceries', checked: false },
-      // { value: 'home-decoration', label: 'home decoration', checked: false },
-      // { value: 'furniture', label: 'furniture', checked: false },
-      // { value: 'tops', label: 'tops', checked: false },
-      // { value: 'womens-dresses', label: 'womens dresses', checked: false },
-      // { value: 'womens-shoes', label: 'womens shoes', checked: false },
-      // { value: 'mens-shirts', label: 'mens shirts', checked: false },
-      // { value: 'mens-shoes', label: 'mens shoes', checked: false },
-      // { value: 'mens-watches', label: 'mens watches', checked: false },
-      // { value: 'womens-watches', label: 'womens watches', checked: false },
-      // { value: 'womens-bags', label: 'womens bags', checked: false },
-      // { value: 'womens-jewellery', label: 'womens jewellery', checked: false },
-      // { value: 'sunglasses', label: 'sunglasses', checked: false },
-      // { value: 'automotive', label: 'automotive', checked: false },
-      // { value: 'motorcycle', label: 'motorcycle', checked: false },
-      // { value: 'lighting', label: 'lighting', checked: false },
+      { value: 'Graphic Designing', label: 'Graphic Designing', checked: false },
+      { value: 'Gaming', label: 'Gaming', checked: false },
     ],
   },
   {
@@ -63,133 +48,47 @@ const filters = [
       { value: 'HP', label: 'HP', checked: false },
       { value: 'MSI', label: 'MSI', checked: false },
       { value: 'ASUS', label: 'ASUS', checked: false },
-       
-      // {
-      //   value: 'Microsoft Surface',
-      //   label: 'Microsoft Surface',
-      //   checked: false,
-      // },
-      // { value: 'Infinix', label: 'Infinix', checked: false },
-      // { value: 'HP Pavilion', label: 'HP Pavilion', checked: false },
-      // {
-      //   value: 'Impression of Acqua Di Gio',
-      //   label: 'Impression of Acqua Di Gio',
-      //   checked: false,
-      // },
-      // { value: 'Royal_Mirage', label: 'Royal_Mirage', checked: false },
-      // {
-      //   value: 'Fog Scent Xpressio',
-      //   label: 'Fog Scent Xpressio',
-      //   checked: false,
-      // },
-      // { value: 'Al Munakh', label: 'Al Munakh', checked: false },
-      // { value: 'Lord - Al-Rehab', label: 'Lord   Al Rehab', checked: false },
-      // { value: "L'Oreal Paris", label: "L'Oreal Paris", checked: false },
-      // { value: 'Hemani Tea', label: 'Hemani Tea', checked: false },
-      // { value: 'Dermive', label: 'Dermive', checked: false },
-      // { value: 'ROREC White Rice', label: 'ROREC White Rice', checked: false },
-      // { value: 'Fair & Clear', label: 'Fair & Clear', checked: false },
-      // { value: 'Saaf & Khaas', label: 'Saaf & Khaas', checked: false },
-      // { value: 'Bake Parlor Big', label: 'Bake Parlor Big', checked: false },
-      // {
-      //   value: 'Baking Food Items',
-      //   label: 'Baking Food Items',
-      //   checked: false,
-      // },
-      // { value: 'fauji', label: 'fauji', checked: false },
-      // { value: 'Dry Rose', label: 'Dry Rose', checked: false },
-      // { value: 'Boho Decor', label: 'Boho Decor', checked: false },
-      // { value: 'Flying Wooden', label: 'Flying Wooden', checked: false },
-      // { value: 'LED Lights', label: 'LED Lights', checked: false },
-      // { value: 'luxury palace', label: 'luxury palace', checked: false },
-      // { value: 'Golden', label: 'Golden', checked: false },
-      // {
-      //   value: 'Furniture Bed Set',
-      //   label: 'Furniture Bed Set',
-      //   checked: false,
-      // },
-      // { value: 'Ratttan Outdoor', label: 'Ratttan Outdoor', checked: false },
-      // { value: 'Kitchen Shelf', label: 'Kitchen Shelf', checked: false },
-      // { value: 'Multi Purpose', label: 'Multi Purpose', checked: false },
-      // { value: 'AmnaMart', label: 'AmnaMart', checked: false },
-      // {
-      //   value: 'Professional Wear',
-      //   label: 'Professional Wear',
-      //   checked: false,
-      // },
-      // { value: 'Soft Cotton', label: 'Soft Cotton', checked: false },
-      // { value: 'Top Sweater', label: 'Top Sweater', checked: false },
-      // {
-      //   value: 'RED MICKY MOUSE..',
-      //   label: 'RED MICKY MOUSE..',
-      //   checked: false,
-      // },
-      // { value: 'Digital Printed', label: 'Digital Printed', checked: false },
-      // { value: 'Ghazi Fabric', label: 'Ghazi Fabric', checked: false },
-      // { value: 'IELGY', label: 'IELGY', checked: false },
-      // { value: 'IELGY fashion', label: 'IELGY fashion', checked: false },
-      // {
-      //   value: 'Synthetic Leather',
-      //   label: 'Synthetic Leather',
-      //   checked: false,
-      // },
-      // {
-      //   value: 'Sandals Flip Flops',
-      //   label: 'Sandals Flip Flops',
-      //   checked: false,
-      // },
-      // { value: 'Maasai Sandals', label: 'Maasai Sandals', checked: false },
-      // { value: 'Arrivals Genuine', label: 'Arrivals Genuine', checked: false },
-      // { value: 'Vintage Apparel', label: 'Vintage Apparel', checked: false },
-      // { value: 'FREE FIRE', label: 'FREE FIRE', checked: false },
-      // { value: 'The Warehouse', label: 'The Warehouse', checked: false },
-      // { value: 'Sneakers', label: 'Sneakers', checked: false },
-      // { value: 'Rubber', label: 'Rubber', checked: false },
-      // { value: 'Naviforce', label: 'Naviforce', checked: false },
-      // { value: 'SKMEI 9117', label: 'SKMEI 9117', checked: false },
-      // { value: 'Strap Skeleton', label: 'Strap Skeleton', checked: false },
-      // { value: 'Stainless', label: 'Stainless', checked: false },
-      // { value: 'Eastern Watches', label: 'Eastern Watches', checked: false },
-      // { value: 'Luxury Digital', label: 'Luxury Digital', checked: false },
-      // { value: 'Watch Pearls', label: 'Watch Pearls', checked: false },
-      // { value: 'Bracelet', label: 'Bracelet', checked: false },
-      // { value: 'LouisWill', label: 'LouisWill', checked: false },
-      // { value: 'Copenhagen Luxe', label: 'Copenhagen Luxe', checked: false },
-      // { value: 'Steal Frame', label: 'Steal Frame', checked: false },
-      // { value: 'Darojay', label: 'Darojay', checked: false },
-      // {
-      //   value: 'Fashion Jewellery',
-      //   label: 'Fashion Jewellery',
-      //   checked: false,
-      // },
-      // { value: 'Cuff Butterfly', label: 'Cuff Butterfly', checked: false },
-      // {
-      //   value: 'Designer Sun Glasses',
-      //   label: 'Designer Sun Glasses',
-      //   checked: false,
-      // },
-      // { value: 'mastar watch', label: 'mastar watch', checked: false },
-      // { value: 'Car Aux', label: 'Car Aux', checked: false },
-      // { value: 'W1209 DC12V', label: 'W1209 DC12V', checked: false },
-      // { value: 'TC Reusable', label: 'TC Reusable', checked: false },
-      // { value: 'Neon LED Light', label: 'Neon LED Light', checked: false },
-      // {
-      //   value: 'METRO 70cc Motorcycle - MR70',
-      //   label: 'METRO 70cc Motorcycle   MR70',
-      //   checked: false,
-      // },
-      // { value: 'BRAVE BULL', label: 'BRAVE BULL', checked: false },
-      // { value: 'shock absorber', label: 'shock absorber', checked: false },
-      // { value: 'JIEPOLLY', label: 'JIEPOLLY', checked: false },
-      // { value: 'Xiangle', label: 'Xiangle', checked: false },
-      // {
-      //   value: 'lightingbrilliance',
-      //   label: 'lightingbrilliance',
-      //   checked: false,
-      // },
-      // { value: 'Ifei Home', label: 'Ifei Home', checked: false },
-      // { value: 'DADAWU', label: 'DADAWU', checked: false },
-      // { value: 'YIOSI', label: 'YIOSI', checked: false },
+      { value: 'SAMSUNG', label: 'SAMSUNG', checked: false },
+      { value: 'Apple', label: 'Apple', checked: false },
+      { value: 'DELL', label: 'DELL', checked: false },
+      { value: 'Acer', label: 'Acer', checked: false },
+
+    ],
+  },
+  {
+    id: 'RamType',
+    name: 'Ram Type',
+    options: [
+      { value: 'SSD', label: 'SSD', checked: false },
+      { value: 'HDD', label: 'HDD', checked: false },
+    ],
+  },
+  {
+    id: 'Processor',
+    name: 'Processor',
+    options: [
+      { value: 'Intel', label: 'Intel', checked: false },
+      { value: 'Ryzen', label: 'Ryzen', checked: false },
+    ],
+  },
+  {
+    id: 'screenSize',
+    name: 'Screen Size',
+    options: [
+      { value: 13 , label: '13 inch', checked: false },
+      { value: 13.3, label: '13.3 inch', checked: false },
+      { value: 14, label: '14 inch', checked: false },
+      { value: 15, label: '15 inch', checked: false },
+      { value: 15.6, label: '15.6 inch', checked: false },
+      { value: 17, label: '17 inch', checked: false },
+    ],
+  },
+  {
+    id: 'operatingSystem',
+    name: 'Operating System',
+    options: [
+      { value: 'Windows', label: 'Windows', checked: false },
+      { value: 'Mac Os', label: 'Mac Os', checked: false },
     ],
   },
 ];
@@ -623,16 +522,23 @@ function ProductGrid({ products }) {
                 </div>
                 <div className="mt-4 flex justify-between">
                   <div>
-                    <h3 className="text-sm text-gray-700">
+                    <h3 className="text-sm text-black-800">
                       <div href={product.href}>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {product.title}
                       </div>
                     </h3>
-                    <p className="mt-1 text-sm text-gray-500">
+                    <p className="mt-1 text-sm text-gray-600">
                       <StarIcon className="w-6 h-6 inline"></StarIcon>
                       <span className=" align-bottom">{product.rating}</span>
                     </p>
+                    <p className="mt-1 text-sm text-gray-600">- {product.screenSize} inch </p>
+                    <p className="mt-1 text-sm text-gray-600">- {product.ram}GB Ram</p>
+                    <p className="mt-1 text-sm text-gray-600">- {product.storage}GB Storage </p>
+                    <p className="mt-1 text-sm text-gray-600">- {product.RamType} </p>
+                    <p className="mt-1 text-sm text-gray-600">- {product.Processor} Processor </p>
+                    <p className="mt-1 text-sm text-gray-600">- {product.operatingSystem} OS </p>
+
                   </div>
                   <div>
                     <p className="text-sm block font-medium text-gray-900">
